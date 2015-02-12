717cee60900d860a6f1a3b2b0d3f830c2c72df31
metinblah <mlblair@fullsail.edu>
2015-02-12 11:53:21 -0500

    Sparky's Age

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by MetinBlah on 2/12/15.
+ */
+//Dog Years
+
+var sparkyAge = 5;
+var result = sparkyAge * 7;
+console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");

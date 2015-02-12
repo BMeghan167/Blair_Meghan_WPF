fc9acd377ac168e76b8975c618ea9ca4ac5e0d81
metinblah <mlblair@fullsail.edu>
2015-02-12 11:56:17 -0500

    Sparky's Age2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,6 +3,7 @@
  */
 //Dog Years
 
-var sparkyAge = 5;
-var result = sparkyAge * 7;
+var sparkyAge = 5; //Variable sets Sparky's human age to 5
+var result = sparkyAge * 7; //Variable takes Sparky's human age and multiplies it with 7
 console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");
+//Using strings and variables to put the printed message together

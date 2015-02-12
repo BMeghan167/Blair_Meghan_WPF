6b2687476ffd75b8e228aee7e4d75fcf1963882e
metinblah <mlblair@fullsail.edu>
2015-02-12 14:14:51 -0500

    Expressions Assignment3
    I changed it a bit to add prompts.

--- Blair_Meghan_Expressions_Assignment/js/script.js
+++ Blair_Meghan_Expressions_Assignment/js/script.js
@@ -2,18 +2,25 @@
 //February 12, 2015
 //Expressions Assignment
 
-var butterscotch = [325, 167, 256];
 var taffy = [413, 297, 541];
-var tootsies = [178, 662, 489];
 
-var totalButter = butterscotch[0] + butterscotch[1] + butterscotch[2];
 var totalTaffy = taffy[0] + taffy[1] + taffy[2];
-var totalTootsie = tootsies[0] + tootsies[1] + tootsies[2];
 
-var totalCandy = totalButter + totalTaffy + totalTootsie;
-var totalAfterKids = totalCandy - tootsies[0];
+var taffyBought = prompt("How much taffy would you like to buy on Monday?");
+var taffyBought2 = prompt("How much taffy would you like to buy on Wednesday?");
+var taffyBought3 = prompt("How much taffy would you like to buy on Friday?");
+console.log(taffyBought);
+console.log(taffyBought2);
+console.log(taffyBought3);
 
-console.log(totalCandy);
-console.log(totalAfterKids);
-console.log("The total amount of candy at opening is" + " " + totalCandy + " ." +
-"After the children's purchase of 178 tootsie rolls the total is" + " " + totalAfterKids + ".");
+var yourTotal = taffyBought + taffyBought2 + taffyBought3;
+
+var totalEnd = totalTaffy - yourTotal;
+
+console.log(totalTaffy);
+console.log(totalEnd);
+console.log("The total amount of taffy at opening is" + " " + totalTaffy + "." +
+"After your three purchases the total is" + " " + totalEnd + ".");
+
+/* I changed 413 to 100 and got an answers of . So this will work for any other values. */
+

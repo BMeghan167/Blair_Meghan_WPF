9c545dff81d841326a65f3bb38372e6928fc1a2c
metinblah <mlblair@fullsail.edu>
2015-02-12 13:22:34 -0500

    Grocery Budget

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,13 +1,17 @@
 /**
  * Created by MetinBlah on 2/12/15.
  */
-//Slice of Pie Part 1
+//Average Shopping Bill
 
-var perPizza = 8; //Variable set for number of slices per pizza
-var guests = 10; //Variable set for number of guests at party
-var totalPizzas = 5; //Variable set for number of pizzas ordered
+var groceryMoney = [240.37, 170.56, 200.49, 110.32, 130.98]; //0,1,2,3,4 //array of amounts spent each week on groceries
 
-var result = perPizza * totalPizzas/guests; //result of multiplying slices of pizza
-// by the number of pizzas ordered and then dividing that number by the number of guests
-console.log("Each person ate" + " " + result + " " + "slices of pizza at the party.");
-//used strings and variables againg to print the statement above
+var total = groceryMoney[0] + groceryMoney[1] + groceryMoney[2] + groceryMoney[3] + groceryMoney[4];
+//variable created to add each number together and get a total from it
+
+var average = total/5 //variable set to divide the total by 5 weeks to find an average
+
+console.log(total); //print total in browser
+console.log(average); //print average in browser
+console.log("You have spent a total of" + " " + "$" + total + " " + "on groceries over 5 weeks." +
+"Thats is an average of" + " " + "$" + average + " " + "per week.")
+//prints phrase with both the total and average in it

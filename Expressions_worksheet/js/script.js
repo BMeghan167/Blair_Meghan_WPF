9caa6d73bb03ea930033d4681deb583a9265ed6e
metinblah <mlblair@fullsail.edu>
2015-02-12 12:08:17 -0500

    Slice of Pie Part 1

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,9 +1,13 @@
 /**
  * Created by MetinBlah on 2/12/15.
  */
-//Dog Years
+//Slice of Pie Part 1
 
-var sparkyAge = 5; //Variable sets Sparky's human age to 5
-var result = sparkyAge * 7; //Variable takes Sparky's human age and multiplies it with 7
-console.log("Sparky is" + " " + sparkyAge + " " + "human years old which is" + " " + result + " " + "in dog years.");
-//Using strings and variables to put the printed message together
+var perPizza = 8; //Variable set for number of slices per pizza
+var guests = 10; //Variable set for number of guests at party
+var totalPizzas = 5; //Variable set for number of pizzas ordered
+
+var result = perPizza * totalPizzas/guests; //result of multiplying slices of pizza
+// by the number of pizzas ordered and then dividing that number by the number of guests
+console.log("Each person ate" + " " + result + " " + "slices of pizza at the party.");
+//used strings and variables againg to print the statement above

26907a2df62ff76908ba11351b61e6a32a2fd9e1
metinblah <mlblair@fullsail.edu>
2015-02-19 18:42:31 -0500

    Conditional Logic2

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,10 +1,11 @@
 //Conditional Logic
 
-var oldEnough = true;
+var kidHeight = 30;
+var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
-
-if(oldEnough){
+// if the kid is over 48" in height
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }

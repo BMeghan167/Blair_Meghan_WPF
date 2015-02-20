b482013783cb99e282ec120ccccbd8fe1d78e96e
metinblah <mlblair@fullsail.edu>
2015-02-19 20:37:23 -0500

    Conditional Logic - Ternary Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,12 +1,26 @@
-//Conditional Logic - Logical Operators
+//Conditional Logic - Ternary Operators
 
-var budget = 100;
-var iPhonePrice = 199.99;
-var wonLottery = true;
+var gpa = 48;
 
-//if the price of the phone is les than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget || wonLottery){
-    console.log("We can buy the phone!");
+//if the gpa is over the min 2.0 score, the student can gradate
+
+/*if(gpa > 2.0){
+    console.log("You can graduate!");
 }else{
-    console.log("No phone for you!!");
+    console.log("GPA is too low!");
+}*/
+
+//(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
+
+var age = 11
+var book;
+
+//if child is under 10, they get green eggs and ham, otherwise, they get the time machine
+/*if(age < 10){
+    book = "Green Eggs and Ham";
+}else{
+    book = "The Time Machine";
 }
+console.log(book);*/
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);

3757159787052ba0fc7c4c322d502b47e6ffde97
metinblah <mlblair@fullsail.edu>
2015-02-19 22:57:17 -0500

    Conditionals Assignment1

---
+++ Blair_Meghan_Conditionals_Assignment/js/script.js
@@ -0,0 +1,26 @@
+//Meghan Blair
+//February 19, 2015
+//Conditionals Assignment
+
+var stockTotal = 12;
+var sarahPurse = prompt("How many purses did Sarah purchase?");
+var sallyPurse = prompt("How many purses did Sally purchase?");
+var susanPurse = prompt("How many purses did Susan purchase?");
+
+if(sarahPurse <= ""){
+    console.log("You didn't tell me how many Sarah bought!");
+}
+if(sallyPurse <= ""){
+    console.log("You didn't tell me how many Sally bought!");
+}
+if(susanPurse <= ""){
+    console.log("You didn't tell me how many Susan bought!");
+}
+
+var totalPurse = sarahPurse + sallyPurse + susanPurse;
+
+if(totalPurse < stockTotal){
+    console.log("Everyone will get their product on time!");
+}else{
+    console.log("You have to wait for restocking!");
+}

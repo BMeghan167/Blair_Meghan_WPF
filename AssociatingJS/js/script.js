6c1dd22377903458522f930d2e46b977c442145d
metinblah <mlblair@fullsail.edu>
2015-02-26 11:02:27 -0500

    Functions - Arguments and Parameters

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,13 +1,8 @@
-//Functions - Variable Scope
+//Functions - Parameter and Arguements
 
-var width = 5;
+calcArea(30, 20);
 
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
+function calcArea(w, h){ //w=30 h=20
+    var area = w * h;
     console.log(area);
 }
-
-calcArea();
-console.log(width);

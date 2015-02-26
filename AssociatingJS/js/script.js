3c73d41e311fed8498620ec57d061407ed1324eb
metinblah <mlblair@fullsail.edu>
2015-02-26 10:49:55 -0500

    Functions - Basic Structure

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,26 +1,12 @@
-//Conditional Logic - Ternary Operators
+//Functions - Basic Structure
 
-var gpa = 48;
-
-//if the gpa is over the min 2.0 score, the student can gradate
-
-/*if(gpa > 2.0){
-    console.log("You can graduate!");
-}else{
-    console.log("GPA is too low!");
-}*/
-
-//(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA too low!");
-
-var age = 11
-var book;
-
-//if child is under 10, they get green eggs and ham, otherwise, they get the time machine
-/*if(age < 10){
-    book = "Green Eggs and Ham";
-}else{
-    book = "The Time Machine";
+function outputMsg(){
+    console.log("Hello World!");
 }
-console.log(book);*/
-book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
-console.log(book);
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}

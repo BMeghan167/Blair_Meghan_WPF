161f4d7871e71694ff135f01b4b27c1ac8162bc7
metinblah <mlblair@fullsail.edu>
2015-02-26 12:13:56 -0500

    Do While Loop

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -6,3 +6,13 @@ while (b > 0){ //checks the condition
     console.log(b + "kegs on the wall");
     b--; //increments or decrements the index
 }
+
+//Do While Loop
+
+var c = 10;
+
+do{
+    console.log(c + "kegs on the wall");
+    c--;
+}
+while(c > 0);

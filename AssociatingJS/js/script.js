0d3f7447a5e5ef5d036c19e2c5c4626a8bc12199
metinblah <mlblair@fullsail.edu>
2015-02-26 11:08:06 -0500

    Functions - Arguments and Parameters 2

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,8 +1,8 @@
 //Functions - Parameter and Arguements
 
-calcArea(30, 20);
-
-function calcArea(w, h){ //w=30 h=20
-    var area = w * h;
-    console.log(area);
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
 }
+dogYears(4);
+dogYears(5);

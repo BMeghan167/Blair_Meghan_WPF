2e0a91f3fda5cb28b292684f469e4a069fd9f31d
metinblah <mlblair@fullsail.edu>
2015-03-09 12:34:04 -0400

    Expressions w/ Arithmetic Operators

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -5,7 +5,14 @@ var b;
 b = a + 3;
 console.log(a);*/
 
-var yearBorn = 1974;
+/*var yearBorn = 1974;
 var age = 2012 - yearBorn;
-console.log(age);
+console.log(age);*/
 
+//Expressions - Arithmetic Operators
+
+//area of the triangle is half of the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);

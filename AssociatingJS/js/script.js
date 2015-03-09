18865b2be697e466199343f5377ee9c0af5b08f7
metinblah <mlblair@fullsail.edu>
2015-03-09 12:00:21 -0400

    Expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,24 +1,4 @@
-//While Loop
+//Expressions
 
-var b = 50; //setup the index
-
-while (b > 0){ //checks the condition
-    console.log(b + "kegs on the wall");
-    b--; //increments or decrements the index
-}
-
-//Do While Loop
-
-var c = 10;
-
-do{
-    console.log(c + "kegs on the wall");
-    c--;
-}
-while(c > 0);
-
-//For Loop
-
-for (var i = 10; i > 0; i--){
-    console.log(i + "kegs on the wall");
-}
+var a = 2
+console.log(a);

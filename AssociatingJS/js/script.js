80da9c3808bd93a270bb5712b72f20c0ef1ed50b
metinblah <mlblair@fullsail.edu>
2015-03-09 12:29:11 -0400

    Expressions w/ Age

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,7 +1,11 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2;
+/*var a = 2;
 var b;
 b = a + 3;
-console.log(a);
+console.log(a);*/
+
+var yearBorn = 1974;
+var age = 2012 - yearBorn;
+console.log(age);
 

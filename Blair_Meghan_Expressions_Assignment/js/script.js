61d04a5a31a2f42cf3fa20a6a355c85096ce8308
BMeghan167 <mlblair@fullsail.edu>
2015-03-12 23:34:16 -0400

    Expressions Assignment 4
    March 12, 2015

--- Blair_Meghan_Expressions_Assignment/js/script.js
+++ Blair_Meghan_Expressions_Assignment/js/script.js
@@ -8,13 +8,15 @@ var horrorMovies = prompt("How many horror movies would you like to rent?"); //c
 var comedyMovies = prompt("How many comedy movies would you like to rent?"); //creates a variable from the users input of comedy movies they rent
 var actionMovies = prompt("Last question, I swear! \n How many action movies would you like to rent?"); //creates a variable from the users input of how many action movies they want to rent
 
-var totalMovies = movieTypes[0] + movieTypes[1] + movieTypes[2];
-console.log("There are a total of" + " " + totalMovies + " " + "movies.");
-var yourTotal = Number(horrorMovies) + Number(comedyMovies) + Number(actionMovies);
-console.log("You rented a total of" + " " + yourTotal + " " + "movies.");
+var totalMovies = movieTypes[0] + movieTypes[1] + movieTypes[2]; //creates a variable for the total amount of movies the rental store has by adding the array amounts together
+console.log("There are a total of" + " " + totalMovies + " " + "movies."); //this line prints the result of the equation above using strings
+var yourTotal = Number(horrorMovies) + Number(comedyMovies) + Number(actionMovies); //this line creates a variable of how many movies the user has rented total so it can be used to subtract from the totalMovies variable later
+console.log("You rented a total of" + " " + yourTotal + " " + "movies."); //this is printing the result of how many movies the user is renting using strings
 
-var newMovieTotal = totalMovies -= yourTotal;
-console.log("The total of movies the store owner has left after you pick your movies is" + " " + newMovieTotal + ".");
+var newMovieTotal = totalMovies -= yourTotal; //this new variable is going to be how many movies are left after the user rents theirs by subtracting yourTotal from totalMovies
+console.log("The total of movies the store owner has left after you pick your movies is" + " " + newMovieTotal + "."); //this line prints the final total of movies after the user has rented theirs using strings
+
+/*Even if you change the number of movies rented, the answer will be the same.*/
 
 
 

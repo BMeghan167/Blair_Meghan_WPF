57318dd60d7f7987f14c71db63231bb4feddbf88
metinblah <mlblair@fullsail.edu>
2015-03-12 22:37:06 -0400

    Expressions Assigment
    Please ignore all of the deletions from the previous expressions
    assignment I did. I don’t understand why it’s showing it.

--- Blair_Meghan_Expressions_Assignment/js/script.js
+++ Blair_Meghan_Expressions_Assignment/js/script.js
@@ -1,24 +1,13 @@
 //Meghan Blair
-//February 12, 2015
 //Expressions Assignment
+//March 12, 2015
 
-var taffy = [413, 297, 541]; //created an array to make a total amount of candy
+var movieTypes = [121, 93, 74]; //creating an array for the movie types the rental store carries
 
-var totalTaffy = taffy[0] + taffy[1] + taffy[2]; //added each number from the array to get a total
+var horrorMovies = prompt("How many horror movies would you like to rent?"); //creating a variable from the users prompt of how many horror movies they want to rent
+var comedyMovies = prompt("How many comedy movies would you like to rent?"); //creates a variable from the users input of comedy movies they rent
+var actionMovies = prompt("Last question, I swear! \n How many action movies would you like to rent?"); //creates a variable from the users input of how many action movies they want to rent
 
-var taffyBought = prompt("How much taffy would you like to buy on Monday?"); //prompts the user to give an amount for the first given
-var taffyBought2 = prompt("How much taffy would you like to buy on Wednesday?"); //prompts the user to give a second amount
-var taffyBought3 = prompt("How much taffy would you like to buy on Friday?"); //prompts the user to give a final amount
-console.log(taffyBought); //prints the amount given by the user
-console.log(taffyBought2); //prints the second amount given by the user
-console.log(taffyBought3); //prints the third amount given by the user
 
-var totalEnd = totalTaffy - (Number(taffyBought) + Number(taffyBought2) + Number(taffyBought3)); //gives you a final total by adding the amounts given by the user and then subtracting them from the beginning total of taffy
 
-console.log(totalTaffy); //prints the total amount of taffy
-console.log(totalEnd); //prints the final total after taking away the amounts given by the user
-console.log("The total amount of taffy at opening is" + " " + totalTaffy + "." + " " +
-"After your three purchases the total is" + " " + totalEnd + "."); //prints a clarified phrase explaining the totals
-
-/* I changed 413 to 100 and got a totalTaffy of 938. So this will work for any other values. */
 

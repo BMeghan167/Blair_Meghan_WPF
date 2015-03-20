34302664041652c187c2a574d2db62503eba86b9
BMeghan167 <mlblair@fullsail.edu>
2015-03-19 20:41:23 -0400

    Conditionals Assignment
    March 19, 2015
    Please ignore the deletions made from my previous assignment, thank you!

--- Blair_Meghan_Conditionals_Assignment/js/script.js
+++ Blair_Meghan_Conditionals_Assignment/js/script.js
@@ -1,33 +1,22 @@
 //Meghan Blair
-//February 19, 2015
+//March 19,2015
 //Conditionals Assignment
 
-var stockTotal = 12; //This is the variable I have set no matter how many purses each person buys
-var sarahPurse = prompt("How many purses did Sarah purchase?"); //This will prompt the user to give an amount of purses that Sarah will buy
-var sallyPurse = prompt("How many purses did Sally purchase?"); //This will prompt for an amount for Sally
-var susanPurse = prompt("How many purses did Susan purchase?"); //This will prompt for an amount for Susan
+var conBudget = 400;
+var nextPaycheck = 300;
 
-//Below, a conditional statement to validate the prompt for Sarah
-if(sarahPurse <= ""){
-    console.log("You didn't tell me how many Sarah bought!");
+var conTicket = prompt("How much is your ticket?");
+var yourCosplay = prompt("How much will you spend on your cosplay?");
+var yourSpending = prompt("How much do you want to spend at Colossalcon?");
+
+if(conTicket === ""){
+    prompt("You forgot to tell me how much your ticket is!");
+    if(yourCosplay === "") {
+        prompt("You forgot to tell me how much your cosplay is going to cost!");
+    }
+    if(yourSpending === "") {
+        prompt("Your forgot to tell me how much you're spending at the convention!");
+    }
+}else{
+    console.log("Keep going!");
 }
-if(sallyPurse <= ""){ //This conditional will validate the prompt for Sally
-    console.log("You didn't tell me how many Sally bought!");
-}
-if(susanPurse <= ""){ //This conditional will validate the prompt for Susan
-    console.log("You didn't tell me how many Susan bought!");
-}
-
-if(Number(sarahPurse) && Number(sallyPurse) <= stockTotal){ //This conditional will use a Logical Operator to use the values for Sarah and Sally to see if they are less than the given stock total
-    console.log("Sarah and Sally will get their purse!");
-}else if(Number(susanPurse) > stockTotal) { //this else if statement will check to see if the amount from susan is greater than the stock total
-    console.log("Susan won't get her purse!");
-}else{ //and this else is for the otherwise of sarah and sally getting too many purses
-    console.log("Sorry you'll have to wait!");
-}
-
-//Now we will use a ternary conditional statement to find how how many purses Sarah, Sally, and Susan want total and if it is less than or equal to the stock total
-var totalPurse;
- totalPurse = (Number(sarahPurse) + Number(sallyPurse) + Number(susanPurse) <= stockTotal) ? "Everyone will get their purses!" : "Oh no! You will have to wait for restocking!";
-
-

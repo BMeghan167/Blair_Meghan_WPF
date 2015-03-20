e8cb88a7c2cb0fb5676c9e3d9d445ce3a452a94a
BMeghan167 <mlblair@fullsail.edu>
2015-03-19 20:50:18 -0400

    Conditionals Assignment 2
    March 19, 2015

--- Blair_Meghan_Conditionals_Assignment/js/script.js
+++ Blair_Meghan_Conditionals_Assignment/js/script.js
@@ -11,12 +11,18 @@ var yourSpending = prompt("How much do you want to spend at Colossalcon?");
 
 if(conTicket === ""){
     prompt("You forgot to tell me how much your ticket is!");
-    if(yourCosplay === "") {
+    if(yourCosplay === ""){
         prompt("You forgot to tell me how much your cosplay is going to cost!");
     }
-    if(yourSpending === "") {
+    if(yourSpending === ""){
         prompt("Your forgot to tell me how much you're spending at the convention!");
     }
 }else{
     console.log("Keep going!");
+}
+
+if(conTicket + yourCosplay + yourSpending <= conBudget + nextPaycheck){
+    console.log("You're good to go!");
+}else{
+    console.log("You might want to cut back somewhere!");
 }

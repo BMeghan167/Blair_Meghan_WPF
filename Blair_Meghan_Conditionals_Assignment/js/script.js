550f562fd8355eef564860bc27ef3c174d374bf9
BMeghan167 <mlblair@fullsail.edu>
2015-03-19 21:06:17 -0400

    Conditionals Assignment 3
    March 19, 2015

--- Blair_Meghan_Conditionals_Assignment/js/script.js
+++ Blair_Meghan_Conditionals_Assignment/js/script.js
@@ -2,8 +2,9 @@
 //March 19,2015
 //Conditionals Assignment
 
-var conBudget = 400;
-var nextPaycheck = 300;
+var conBudget = 400; //This will set a variable for your convention budget at $400
+var nextPaycheck = 300; //This will set a variable for your next paycheck at $300 that is also of use to you for the convention
+var rentPaid = true; //This is a boolean that will add to a conditional below
 
 var conTicket = prompt("How much is your ticket?");
 var yourCosplay = prompt("How much will you spend on your cosplay?");
@@ -21,8 +22,11 @@ if(conTicket === ""){
     console.log("Keep going!");
 }
 
-if(conTicket + yourCosplay + yourSpending <= conBudget + nextPaycheck){
-    console.log("You're good to go!");
+if(Number(conTicket) + Number(yourCosplay) + Number(yourSpending) <= conBudget + nextPaycheck){
+    console.log("You have enough money!");
+}else if(rentPaid){
+    console.log("No worries! Your bills are paid!");
 }else{
     console.log("You might want to cut back somewhere!");
 }
+
